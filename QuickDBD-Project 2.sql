6e7c3345f251db0f75d0408c03369ee1ef5771ca
ZHAO W <54wenzhao@gmail.com>
2023-11-13 20:43:18 -0500

    added sql file. update notebook

---
+++ QuickDBD-Project 2.sql
@@ -0,0 +1,60 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/NpyiG2
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "compaign" (
+    "cd_id" INT   NOT NULL,
+    "contact_id" INT   NOT NULL,
+    "company_name" VARCHAR(25)   NOT NULL,
+    "description" VARCHAR(25)   NOT NULL,
+    "goal" float   NOT NULL,
+    "pledged" float   NOT NULL,
+    "outcome" VARCHAR(25)   NOT NULL,
+    "backers_count" INT   NOT NULL,
+    "country" CHAR(2)   NOT NULL,
+    "currency" CHAR(3)   NOT NULL,
+    "launch_date" DATETIME   NOT NULL,
+    "end_date" DATETIME   NOT NULL,
+    "category_id" VARCHAR(5)   NOT NULL,
+    "subcategory_id" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_compaign" PRIMARY KEY (
+        "cd_id"
+     )
+);
+
+CREATE TABLE "categories" (
+    "category_id" VARCHAR(5)   NOT NULL,
+    "category" VARCHAR(25)   NOT NULL,
+    CONSTRAINT "pk_categories" PRIMARY KEY (
+        "category_id"
+     )
+);
+
+CREATE TABLE "subcategories" (
+    "subcategory_id" VARCHAR(10)   NOT NULL,
+    "subcategories" VARCHAR(25)   NOT NULL,
+    CONSTRAINT "pk_subcategories" PRIMARY KEY (
+        "subcategory_id"
+     )
+);
+
+CREATE TABLE "contact" (
+    "contact_id" int   NOT NULL,
+    "first_name" VARCHAR(25)   NOT NULL,
+    "last_name" VARCHAR(25)   NOT NULL,
+    "email" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_contact" PRIMARY KEY (
+        "contact_id"
+     )
+);
+
+ALTER TABLE "compaign" ADD CONSTRAINT "fk_compaign_contact_id" FOREIGN KEY("contact_id")
+REFERENCES "contact" ("contact_id");
+
+ALTER TABLE "compaign" ADD CONSTRAINT "fk_compaign_category_id" FOREIGN KEY("category_id")
+REFERENCES "categories" ("category_id");
+
+ALTER TABLE "compaign" ADD CONSTRAINT "fk_compaign_subcategory_id" FOREIGN KEY("subcategory_id")
+REFERENCES "subcategories" ("subcategory_id");
+
